c311068692a69e8da030a98bd428a35db7da8d7b
ephilosophy <abhimahajan40@gmail.com>
2020-02-18 23:28:18 +0530

    2 files modified

--- pages/score/score.ts
+++ pages/score/score.ts
@@ -8,22 +8,49 @@ import { NavController } from 'ionic-angular';
 export class ScoreCard {
 
   constructor(public navCtrl: NavController) { }
-  overs = [{over:[],runs:0,wickets:0}];  
+  innings = [[{over:[],runs:0,wickets:0}]];
+  innIndex = 0;  
   ballIndex = 0;
   overIndex = 0;
+  result = '';
+  matchComplete = false;
+
   calculateRun(v){
     this.ballIndex += 1;
-    this.overs[thisoverIndex].over.push(v);
+    this.innings[this.innIndex][this.overIndex].over.push(v);
     if(v != 'w'){
-      this.overs[this.overIndex].runs += v;
+      this.innings[this.innIndex][this.overIndex].runs += v;
     }else{
-      this.overs[this.overIndex].wickets += 1;
+      this.innings[this.innIndex][this.overIndex].wickets += 1;
     }
     if(this.ballIndex == 6){
-      var rs = this.overs[this.overIndex].runs, ws = this.overs[this.overIndex].wickets;
+      var rs = this.innings[this.innIndex][this.overIndex].runs, ws = this.innings[this.innIndex][this.overIndex].wickets;
       this.overIndex += 1;
-      this.overs.push({over:[],runs:rs,wickets:ws});      
+      this.innings[this.innIndex].push({over:[],runs:rs,wickets:ws});      
       this.ballIndex = 0;
+    }
+  }
+
+  completeInning(inn){
+    
+    if(inn != 1){
+      this.innings.push([{over:[],runs:0,wickets:0}]);
+      this.innIndex += 1;
+      this.overIndex = 0;
+    }else{
+      this.matchComplete = true;
+      var firstInning = this.innings[0];
+      var secondInning = this.innings[1];
+      var firstInningRuns = firstInning[firstInning.length].runs;
+      var firstInningWickets = firstInning[firstInning.length].wickets;
+      var secondInningRuns = secondInning[secondInning.length].runs;
+      var secondInningWickets = secondInning[secondInning.length].wickets;
+      if(firstInningRuns > secondInningRuns){
+        this.result = "Inning 1 won by "+(firstInningRuns - secondInningRuns)+" runs.";
+      }
+      if(firstInningRuns < secondInningRuns){
+        this.result = "Inning 2 won by "+(10 - secondInningWickets)+" wickets.";
+      }
     }
   }
 
